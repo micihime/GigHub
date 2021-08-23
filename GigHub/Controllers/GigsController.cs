a162c8acd065c724ddde235fc73710c80ac6ea1c
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-08-23 15:25:52 +0200

    Create new gig POST action v1

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -23,6 +25,28 @@ namespace GigHub.Controllers
             };
 
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel vm)
+        {
+            var userId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == userId);
+            var genre = _context.Genres.Single(g => g.Id == vm.Genre);
+
+            var gig = new Gig()
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}", vm.Date, vm.Time)), //for now, assuming user puts in valid data (validation in the next task)
+                Genre = genre,
+                Venue = vm.Venue
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home"); //temporary redirection to homepage
         }
     }
 }
