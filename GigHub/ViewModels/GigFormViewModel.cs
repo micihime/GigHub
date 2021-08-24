a27008cb961ceb285953799f01aeccb9ece0e9bd
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-08-24 11:02:33 +0200

    Refactoring property decl.

--- GigHub/ViewModels/GigFormViewModel.cs
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -11,9 +11,6 @@ namespace GigHub.ViewModels
         public string Venue { get; set; }
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime
-        {
-            get { return DateTime.Parse($"{Date} {Time}"); }
-        }
+        public DateTime DateTime => DateTime.Parse($"{Date} {Time}");
     }
 }
