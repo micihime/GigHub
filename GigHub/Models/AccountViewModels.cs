dab79bae271c37265063f741168018433c66cc1a
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-08-27 14:56:25 +0200

    Adding Name to registration form

--- GigHub/Models/AccountViewModels.cs
+++ GigHub/Models/AccountViewModels.cs
@@ -70,6 +70,11 @@ namespace GigHub.Models
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
