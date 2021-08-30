e7a20774750126f5315329118be8c72753d4f9de
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-08-30 09:57:23 +0200

    Adding Attendance

--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace GigHub.Models
     {
         public DbSet<Gig> Gigs { get; set; }
         public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; } //in order to be able to query this db table
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
@@ -16,6 +17,21 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) //overriding this method in order to be able to use Fluent API
+        {
+            //getting rid of ERROR when updating the DB
+            //  Introducing FOREIGN KEY constraint 'FK_dbo.Attendances_dbo.Gigs_GigId' on table 'Attendances' may cause cycles
+            //  or multiple cascade paths. Specify ON DELETE NO ACTION or ON UPDATE NO ACTION, or modify other FOREIGN KEY
+            //  constraints.
+            //removing the cascade on delete for Gigs and Attendance table
+            modelBuilder.Entity<Attendance>().
+                HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
---
+++ GigHub/Models/Attendance.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GigHub.Models
+{
+    public class Attendance
+    {
+        public Gig Gig { get; set; }
+        public ApplicationUser Attendee { get; set; }
+
+        [Key] //creating composite key to identify attendance
+        [Column(Order = 1)] //1st part of the key
+        public int GigId { get; set; }
+
+        [Key] //creating composite key to identify attendance
+        [Column(Order = 2)] //2nd part of the key
+        public string AttendeeId { get; set; }
+    }
+}
