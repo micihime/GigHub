690924adf39300c0e24da0aa3507e1a8aab1ba79
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-09-10 14:43:25 +0200

    Refactoring - 2 roundtrips to DB into 1, adding navig prop to the Gig class

--- GigHub/Controllers/Api/GigsController.cs
+++ GigHub/Controllers/Api/GigsController.cs
@@ -1,5 +1,6 @@
 ﻿using GigHub.Models;
 using Microsoft.AspNet.Identity;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Http;
 
@@ -19,7 +20,9 @@ namespace GigHub.Controllers.Api
         public IHttpActionResult Cancel(int id)
         {
             var userId = User.Identity.GetUserId();
-            var gig = _context.Gigs.Single(g => g.Id == id && g.ArtistId == userId);
+            var gig = _context.Gigs
+                .Include(g => g.Attendances.Select(a => a.Attendee))
+                .Single(g => g.Id == id && g.ArtistId == userId);
 
             if (gig.IsCancelled)
             {
@@ -30,11 +33,7 @@ namespace GigHub.Controllers.Api
 
             var notif = new Notification(NotificationType.GigCancelled, gig);
 
-            var attendees = _context.Attendances.Where(a => a.GigId == gig.Id)
-                .Select(a => a.Attendee)
-                .ToList();
-
-            foreach (var attendee in attendees)
+            foreach (var attendee in gig.Attendances.Select(a => a.Attendee))
             {
                 attendee.Notify(notif);
             }
