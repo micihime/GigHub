6c62104c46725a1ecf07be7b79c1c078326db686
Miroslava Hrešková <miroslava.hreskova@t-systems.com>
2021-09-13 14:22:19 +0200

    Create API endpoint to return notifications

---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,31 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotificationsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<Notification> GetNotifications()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifs = _context.UserNotifications.Where(un => un.UserId == userId)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+            return notifs;
+        }
+    }
+}
